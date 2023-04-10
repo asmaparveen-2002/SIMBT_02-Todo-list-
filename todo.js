9c3a90eaa403478cedfc769e44c50188816c0ab5
asmaparveen-2002 <aasmaparveen1509@gmail.com>
2023-04-10 15:22:09 +0530

    ab

--- todo.js
+++ todo.js
@@ -59,5 +59,12 @@ addButton.addEventListener('click', () => {
 
 
     textInputField.value = "";
+    // Add a "checked" symbol when clicking on a list item
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function(ev) {
+        if (ev.target.tagName === 'LI') {
+         ev.target.classList.toggle('checked');
+        }
+    }, false);
 
 });
